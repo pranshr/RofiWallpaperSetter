d585606598364ad9fc51c3f821523c91517bc8a6
pranshr <pranshr005@gmail.com>
2026-01-12 23:15:06 +0530

    Script now shows the wallpapers instead of just names.

--- wallSetter.sh
+++ wallSetter.sh
@@ -3,7 +3,21 @@
 
 
 dir=~/desktop-bg
-options=$(ls $dir)
+# wallpapers=$(find $dir -mindepth 1 -printf "%P|" | awk '{print substr($0, 1, length($0)-1)}')
 
-selection=$(echo $options | rofi -dmenu -sep ' ')
-wallset $dir/$selection
+declare -A images
+
+options=""
+for file in in "$dir"/*; do
+
+    images[" \0icon\x1f$file"]="$file"
+
+    if [ -z "$options" ]; then
+        options="$file\0icon\x1f$file"
+    else
+        options="$options|$file\0icon\x1f$file"
+    fi
+done
+
+selection=$(echo -e $options | rofi -dmenu -sep '|' -show-icons)
+wallset "$selection"
